2d9be00cff4f1e962d9fa4d286be560ac800b3ba
Artie Kh <khrt@ya.ru>
2013-01-30 13:04:46 +0200

    Moved `read_data` to Kompot::Renderer

--- lib/Kompot/Base.pm
+++ lib/Kompot/Base.pm
@@ -15,8 +15,9 @@ sub new {
     return $self;
 }
 
-# default initializer
 sub init {1}
+
+sub app { state $_app ||= Kompot::App->new }
 
 sub load_package {
     my ($self, $package) = @_;
@@ -32,42 +33,6 @@ sub load_package {
 
     return 1;
 }
-
-sub read_data {
-    my ($self, $class, $data) = @_;
-    state %CACHE;
-
-    # Refresh or use cached data
-    my $handle = do { no strict 'refs'; \*{"${class}::DATA"} };
-    if (not fileno $handle) {
-        return $data ? $CACHE{$class}{$data} : $CACHE{$class} || {};
-    }
-
-    seek $handle, 0, 0;
-    my $content = join '', <$handle>;
-    close $handle;
-
-    # Ignore everything before __DATA__ (Windows will seek to start of file)
-    $content =~ s/^.*\n__DATA__\r?\n/\n/s;
-
-    # Ignore everything after __END__
-    $content =~ s/\n__END__\r?\n.*$/\n/s;
-
-    # Split
-    my @data = split /^@@\s*(.+?)\s*\r?\n/m, $content;
-    shift @data;
-
-    # Find data
-    my $all = $CACHE{$class} = {};
-    while (@data) {
-        my ($name, $content) = splice @data, 0, 2;
-        $all->{$name} = $content;
-    }
-
-    return $data ? $all->{$data} : $all;
-}
-
-sub app { state $_app ||= Kompot::App->new }
 
 1;
 
