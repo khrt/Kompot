b1231831c6860ca15b1bf087e8c76fea4ca22979
Artie Kh <khrt@ya.ru>
2013-04-20 00:11:27 +0300

    updated query string and body params parsing, cookie parsing and etc [#16]

---
+++ lib/Kompot/Template.pm
@@ -0,0 +1,35 @@
+package Kompot::Template;
+
+use strict;
+use warnings;
+
+use utf8;
+use v5.12;
+
+use Carp;
+
+my %TAGS = (
+    start => '<%',
+    end   => '%>',
+);
+
+#sub init {
+#}
+
+sub parse {
+    # 1 parse template
+}
+
+sub build {
+    # 2 generate perl code
+}
+
+sub interpret {
+    # 3 execute code
+}
+
+sub render {
+
+}
+
+1;
